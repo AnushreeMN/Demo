019bbc80faf993c61fb534d66580dec59f1c6386
ravi88 <ravindra.reddy619@gmail.com>
2018-04-27 10:40:34 +0530

    commit 25

---
+++ Demo/src/test/java/pack1/Finaltest.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+@Test
+
+public class Finaltest {
+	public void test()
+	{
+		Reporter.log("hellooo",true);
+	}
+
+}
